ba6b6477870b3d8d42fbeba7b8771df4569afb05
David Murillo <davimuri@gmail.com>
2019-04-02 22:35:19 +0200

    Binary search to find an index to insert an element in a sorted array

---
+++ src/main/java/algorithms/binsearch/BinarySearchToInsertElement.java
@@ -0,0 +1,36 @@
+package algorithms.binsearch;
+
+/**
+ * Find right index to insert an element in a sorted array
+ */
+public class BinarySearchToInsertElement {
+
+    public static int findIndexToInsert(int[] nums, int element) {
+        if (nums == null || nums.length == 0) return 0;
+
+        return binarySearch(nums, 0, nums.length - 1, element);
+    }
+
+    private static int binarySearch(int[] nums, int left, int right, int element) {
+        if (right - left <= 1) {
+            if (element < nums[left]) {
+                return left;
+            } else if (nums[left] <= element && element <= nums[right]){
+                return right;
+            } else if (nums[right] < element) {
+                return right + 1;
+            }
+        }
+        int middle = (left + right) / 2;
+        if (element == nums[middle]) {
+            return middle;
+        }
+        if (element > nums[middle]) {
+            return binarySearch(nums, middle, right, element);
+        }
+        if (element < nums[middle]) {
+            return binarySearch(nums, left, middle, element);
+        }
+        return nums.length;
+    }
+}
---
+++ src/test/java/algorithms/binsearch/BinarySearchToInsertElementTest.java
@@ -0,0 +1,86 @@
+package algorithms.binsearch;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchToInsertElementTest {
+
+    @Test
+    public void testEmptyArray() {
+        int[] nums = {};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 3);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void testArray1Element3() {
+        int[] nums = {1};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 3);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void testArray5Element2() {
+        int[] nums = {5};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 2);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void testBasicCase() {
+        int[] nums = {1, 2, 4, 5};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 3);
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void givenOddArrayFindZeroIndex() {
+        int[] nums = {2, 4, 5};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 1);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void givenEvenArrayFindZeroIndex() {
+        int[] nums = {2, 4, 5, 6};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 1);
+        assertEquals(0, actual);
+    }
+
+    @Test
+    public void givenOddArrayFindMaxIndex() {
+        int[] nums = {2, 4, 5};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 6);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    public void givenEvenArrayFindMaxIndex() {
+        int[] nums = {2, 4, 5, 6};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 8);
+        assertEquals(4, actual);
+    }
+
+    @Test
+    public void givenArrayWithGapFindIndexBetweenGapToTheLeft() {
+        int[] nums = {2, 4, 20, 21, 22};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 10);
+        assertEquals(2, actual);
+    }
+
+    @Test
+    public void givenArrayWithGapFindIndexBetweenGapToTheRight() {
+        int[] nums = {2, 4, 20, 31, 32};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 25);
+        assertEquals(3, actual);
+    }
+
+    @Test
+    public void givenArrayWithDuplicatesFindIndexInDuplicates() {
+        int[] nums = {2, 4, 20, 20, 20, 31, 32};
+        int actual = BinarySearchToInsertElement.findIndexToInsert(nums, 20);
+        assertTrue(2 <= actual && actual <= 5);
+    }
+
+}
